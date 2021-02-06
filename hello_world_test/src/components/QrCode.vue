8097584f72662b4448905bd67d6f54703e170be5
qb1968 <qb1968@aol.com>
2021-02-06 16:47:32 -0500

    Implementing QRCodes

---
+++ hello_world_test/src/components/QrCode.vue
@@ -0,0 +1,50 @@
+<template>
+  
+ <div class="qr_codes">
+ <h5 class="ja">Jonathan Allison</h5>
+  <qrcode-vue  :value="value" :size="size" level="H" />
+  <h5 class="tz">Tezos</h5>
+  <qrcode-vue :value="value1" :size="size1" level="H" />
+  <h5 class="bt">Burst</h5>
+  <qrcode-vue :value="value2" :size="size2" level="H" />
+  <h5 class="co">Cardano</h5>
+  <qrcode-vue :value="value3" :size="size3" level="H" />
+  </div>
+</template>
+<script>
+  import QrcodeVue from 'qrcode.vue'
+
+  export default {
+    data() {
+      return {
+        value: 'https://github.com/qb1968',
+        size: 50,
+        value1: 'https://goldprice.org/cryptocurrency-price/tezos-price',
+        size1: 50,
+        value2: 'https://coinranking.com/coin/LUbkfWw7GwJ73+burstcoin-burst',
+        size2: 50,
+        value3: 'https://goldprice.org/cryptocurrency-price/cardano-price',
+        size3: 50
+      }
+    },
+    components: {
+      QrcodeVue,
+    },
+  }
+</script>
+
+<style>
+
+.qr_codes {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+}
+
+h5 {
+    font-size: 12px;
+}
+
+
+</style>
--- hello_world_test/src/main.js
+++ hello_world_test/src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from  'vue'
 import App from './App.vue'
+ 
 
+// Register the Vue component
+// Vue.component('qr-code', VueQRCodeComponent)
+
+// Create a Vue instance
 createApp(App).mount('#app')
